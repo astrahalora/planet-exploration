d45127743b2e708de6b485d6c67e162388a76862
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-07-12 17:03:53 +0300

    .

--- src/main/java/com/codecool/marsexploration/output/service/MapFileWriterImpl.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.marsexploration.output.service;
-
-import com.codecool.marsexploration.mapelements.model.Map;
-
-public class MapFileWriterImpl implements MapFileWriter{
-    public void writeMapFile(Map map, String file) {
-
-    }
-}
