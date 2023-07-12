70b2e1a870c9c73f5e6e9f3f5c78011fcb6604f9
Astrahalora <astrahalora@gmail.com>
2023-07-12 15:50:25 +0300

    MapGenerator WIP

--- src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImpl.java
+++ src/main/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImpl.java
@@ -1,11 +1,64 @@
 package com.codecool.marsexploration.mapelements.service.generator;
 
+import com.codecool.marsexploration.calculators.model.Coordinate;
+import com.codecool.marsexploration.calculators.service.CoordinateCalculator;
 import com.codecool.marsexploration.configuration.model.MapConfiguration;
 import com.codecool.marsexploration.mapelements.model.Map;
+import com.codecool.marsexploration.mapelements.model.MapElement;
+import com.codecool.marsexploration.mapelements.service.placer.MapElementPlacer;
+
+import java.util.List;
 
 public class MapGeneratorImpl implements MapGenerator{
-    @Override
-    public Map generate(MapConfiguration mapConfig) {
-        return null;
+    private final MapElementsGenerator mapElementsGenerator;
+    private final MapElementPlacer mapElementPlacer;
+    private final CoordinateCalculator coordinateCalculator;
+
+    public MapGeneratorImpl(MapElementsGenerator mapElementsGenerator, MapElementPlacer mapElementPlacer, CoordinateCalculator coordinateCalculator) {
+        this.mapElementsGenerator = mapElementsGenerator;
+        this.mapElementPlacer = mapElementPlacer;
+        this.coordinateCalculator = coordinateCalculator;
+    }
+
+//    public Map generate(MapConfiguration mapConfig) {
+//        String[][] emptyMap = createEmptyStringArray(mapConfig.mapSize(), mapConfig.mapSize());
+//        List<MapElement> mapElements = (List<MapElement>) mapElementsGenerator.createAll(mapConfig);
+//        for (MapElement mapElement : mapElements) {
+//            Coordinate randoCoordinate = coordinateCalculator.getRandomCoordinate(mapElement.getDimension());
+//
+//            if(mapElementPlacer.canPlaceElement(mapElement, emptyMap, randoCoordinate)) {
+//                mapElementPlacer.placeElement(mapElement, emptyMap, randoCoordinate);
+//            } else {
+//
+//            }
+//        }
+//
+//        return null;
+//    }
+public Map generate(MapConfiguration mapConfig) {
+    String[][] mapToFill = createEmptyStringArray(mapConfig.mapSize(), mapConfig.mapSize());
+    List<MapElement> mapElements = (List<MapElement>) mapElementsGenerator.createAll(mapConfig);
+    for (MapElement mapElement : mapElements) {
+        Coordinate randoCoordinate = coordinateCalculator.getRandomCoordinate(mapElement.getDimension());
+//        while(!mapElementPlacer.canPlaceElement(mapElement, mapToFill, randoCoordinate)) {
+//            System.out.println("Trying config...");
+//            randoCoordinate = coordinateCalculator.getRandomCoordinate(mapElement.getDimension());
+//        }
+        mapElementPlacer.placeElement(mapElement, mapToFill, randoCoordinate);
+    }
+
+    return new Map(mapToFill);
+}
+
+    public static String[][] createEmptyStringArray(int rows, int columns) {
+        String[][] array = new String[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[i][j] = "";
+            }
+        }
+
+        return array;
     }
 }
---
+++ src/test/java/com/codecool/marsexploration/mapelements/service/generator/MapGeneratorImplTest.java
@@ -0,0 +1,89 @@
+package com.codecool.marsexploration.mapelements.service.generator;
+
+import com.codecool.marsexploration.calculators.service.CoordinateCalculator;
+import com.codecool.marsexploration.calculators.service.CoordinateCalculatorImpl;
+import com.codecool.marsexploration.calculators.service.DimensionCalculator;
+import com.codecool.marsexploration.calculators.service.DimensionCalculatorImpl;
+import com.codecool.marsexploration.configuration.model.ElementToSize;
+import com.codecool.marsexploration.configuration.model.MapConfiguration;
+import com.codecool.marsexploration.configuration.model.MapElementConfiguration;
+import com.codecool.marsexploration.mapelements.model.Map;
+import com.codecool.marsexploration.mapelements.service.builder.MapElementBuilder;
+import com.codecool.marsexploration.mapelements.service.builder.MapElementBuilderImpl;
+import com.codecool.marsexploration.mapelements.service.placer.MapElementPlacer;
+import com.codecool.marsexploration.mapelements.service.placer.MapElementPlacerImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapGeneratorImplTest {
+
+    @Test
+    void generateMapFor26Elements_returnsTrue() {
+        DimensionCalculator dimensionCalculator = new DimensionCalculatorImpl();
+        MapElementBuilder mapElementBuilder = new MapElementBuilderImpl(dimensionCalculator);
+        MapElementsGenerator mapElementsGenerator = new MapElementsGeneratorImpl(mapElementBuilder);
+        CoordinateCalculator coordinateCalculator = new CoordinateCalculatorImpl(getConfiguration());
+        MapElementPlacer mapElementPlacer = new MapElementPlacerImpl(coordinateCalculator);
+
+        MapGenerator mapGenerator = new MapGeneratorImpl(mapElementsGenerator, mapElementPlacer, coordinateCalculator);
+
+        Map myMap = mapGenerator.generate(getConfiguration());
+        System.out.println(Arrays.deepToString(myMap.getRepresentation()));
+    }
+
+    private static MapConfiguration getConfiguration() {
+        final String mountainSymbol = "#";
+        final String pitSymbol = "&";
+        final String mineralSymbol = "%";
+        final String waterSymbol = "*";
+
+        MapElementConfiguration mountainsCfg = new MapElementConfiguration(
+                mountainSymbol,
+                "mountain",
+                List.of(
+                        new ElementToSize(1, 20),
+                        new ElementToSize(1, 30)
+                ),
+                3,
+                ""
+        );
+
+        MapElementConfiguration pitsCfg = new MapElementConfiguration(
+                pitSymbol,
+                "pit",
+                List.of(
+                        new ElementToSize(1, 10),
+                        new ElementToSize(1, 20)
+                ),
+                10,
+                ""
+        );
+
+        MapElementConfiguration mineralsCfg = new MapElementConfiguration(
+                mineralSymbol,
+                "mineral",
+                List.of(
+                        new ElementToSize(2, 1)
+                ),
+                0,
+                mountainSymbol
+        );
+
+        MapElementConfiguration pocketsOfWaterCfg = new MapElementConfiguration(
+                waterSymbol,
+                "water",
+                List.of(
+                        new ElementToSize(2, 1)
+                ),
+                0,
+                pitSymbol
+        );
+
+        List<MapElementConfiguration> elementsCfg = List.of(mountainsCfg, pitsCfg, mineralsCfg, pocketsOfWaterCfg);
+        return new MapConfiguration(1000, 0.5, elementsCfg);
+    }
+}
